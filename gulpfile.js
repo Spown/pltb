8aef81b60a3c57ae3dd61e6fbb1739d4fe0bc9c4
Spown <spown.05@mail.ru>
2016-03-09 18:30:05 +0100

    + init

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+        watch = require('gulp-watch')
+;
+
+gulp.task('watch:scss', function() {
+    var last = {};
+    watch(
+        './',
+        {'events': ['add', 'change', 'unlink', 'addDir', 'unlinkDir']},
+        function (f) {
+            console.log('scss operation');
+        }
+    )
+    ;
+});
+ 
+gulp.task('default', ['watch:scss']);
